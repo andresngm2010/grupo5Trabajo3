e6785a1e93e1c7b2b285935ad59194b4fd66c46a
andresngm2010 <79234165+andresngm2010@users.noreply.github.com>
2023-02-15 10:47:43 -0500

    commit 2

--- main.py
+++ main.py
@@ -22,14 +22,18 @@ class base_datos:
         relaciones_faltantes = []
         todo = {}
 
-        cursor.execute("SELECT table_name FROM INFORMATION_SCHEMA.TABLES")
+        cursor.execute("SELECT table_name, table_schema FROM INFORMATION_SCHEMA.TABLES")
 
         for c in cursor:
-            t = c.__getattribute__('table_name')
-            tablas.append(t)
+            tn = c.__getattribute__('table_name')
+            ts = c.__getattribute__('table_schema')
+            tablas.append([tn, ts])
 
         for tabla in tablas:
-            cursor.execute("EXEC sp_fkeys '" + tabla + "'")
+            tn = tabla[0]
+            ts = tabla[1]
+            cursor.execute("EXEC sp_fkeys @pktable_name = '"+tn+"' "
+                           ",@pktable_owner = '"+ts+"';")
             for c in cursor:
                 tabla_1 = c.__getattribute__('PKTABLE_NAME')
                 tabla_2 = c.__getattribute__('FKTABLE_NAME')
@@ -66,29 +70,29 @@ class base_datos:
         return [relaciones_existentes, relaciones_faltantes]
 
     def anomalias_triggers(self):
+        cursor = self.cursor
         triggers = {}
 
-        consulta = """SELECT  
-        sysobjects.name AS trigger_name  
-        ,USER_NAME(sysobjects.uid) AS trigger_owner  
-        ,s.name AS table_schema  
-        ,OBJECT_NAME(parent_obj) AS table_name  
-        ,OBJECTPROPERTY( id, 'ExecIsUpdateTrigger') AS isupdate  
-        ,OBJECTPROPERTY( id, 'ExecIsDeleteTrigger') AS isdelete  
-        ,OBJECTPROPERTY( id, 'ExecIsInsertTrigger') AS isinsert  
-        ,OBJECTPROPERTY(id, 'ExecIsTriggerDisabled') AS [disabled]  
-        FROM sysobjects  
-        INNER JOIN sysusers  
-        ON sysobjects.uid = sysusers.uid  
-        INNER JOIN sys.tables t  
-        ON sysobjects.parent_obj = t.object_id  
-        INNER JOIN sys.schemas s  
-        ON t.schema_id = s.schema_id  
-        WHERE sysobjects.type = 'TR'"""
+        consulta = "SELECT sysobjects.name AS trigger_name" \
+                   " ,USER_NAME(sysobjects.uid) AS trigger_owner  " \
+                   ",s.name AS table_schema  " \
+                   ",OBJECT_NAME(parent_obj) AS table_name  " \
+                   ",OBJECTPROPERTY( id, 'ExecIsUpdateTrigger') AS isupdate  " \
+                   ",OBJECTPROPERTY( id, 'ExecIsDeleteTrigger') AS isdelete  " \
+                   ",OBJECTPROPERTY( id, 'ExecIsInsertTrigger') AS isinsert  " \
+                   ",OBJECTPROPERTY(id, 'ExecIsTriggerDisabled') AS [disabled]  " \
+                   "FROM sysobjects  " \
+                   "INNER JOIN sysusers  " \
+                   "ON sysobjects.uid = sysusers.uid  " \
+                   "INNER JOIN sys.tables t  " \
+                   "ON sysobjects.parent_obj = t.object_id  " \
+                   "INNER JOIN sys.schemas s  " \
+                   "ON t.schema_id = s.schema_id  " \
+                   "WHERE sysobjects.type = 'TR'"
 
-        self.cursor.execute(consulta)
+        cursor.execute(consulta)
 
-        for c in self.cursor:
+        for c in cursor:
             key = c.__getattribute__('table_name')
             triggers[key] = []
             if c.__getattribute__('isupdate') == 1:
@@ -105,14 +109,17 @@ class base_datos:
         return triggers
 
     def anomalias_datos(self):
+        cursor = self.cursor
         anomalias = []
 
-        self.cursor.execute('dbcc checkconstraints with all_constraints')
-
-        for c in self.cursor:
-            tabla = c.__getattribute__("Table")
-            error = c.__getattribute__("Where")
-            anomalias.append([tabla, error])
+        try:
+            cursor.execute('dbcc checkconstraints with all_constraints')
+            for c in cursor:
+                tabla = c.__getattribute__("Table")
+                error = c.__getattribute__("Where")
+                anomalias.append([tabla, error])
+        except:
+            anomalias.append("No se han encontrado anomalias")
 
         return anomalias
 
@@ -126,17 +133,17 @@ def analizar_temp():
     triggers_string = str(bdd.anomalias_triggers()).replace("],", "]\n")
     anomalias_string = str(bdd.anomalias_datos()).replace("],", "]\n")
 
-    etiqueta_temp_relaciones_existentes_string = ttk.Label(text=relaciones_existentes_string)
-    etiqueta_temp_relaciones_existentes_string.place(x=20, y=140)
+    etiqueta_temp_relaciones_existentes_string = ttk.Label(text=relaciones_existentes_string, background="bisque")
+    etiqueta_temp_relaciones_existentes_string.grid(column=0, row=3, padx=4, pady=4, sticky="nw")
 
-    etiqueta_temp_relaciones_faltantes_string = ttk.Label(text=relaciones_faltantes_string)
-    etiqueta_temp_relaciones_faltantes_string.place(x=220, y=140)
+    etiqueta_temp_relaciones_faltantes_string = ttk.Label(text=relaciones_faltantes_string, background="bisque")
+    etiqueta_temp_relaciones_faltantes_string.grid(column=1, row=3, padx=4, pady=4, sticky="nw")
 
-    etiqueta_temp_triggers_string = ttk.Label(text=triggers_string)
-    etiqueta_temp_triggers_string.place(x=420, y=140)
+    etiqueta_temp_triggers_string = ttk.Label(text=triggers_string, background="bisque")
+    etiqueta_temp_triggers_string.grid(column=2, row=3, padx=4, pady=4, sticky="nw")
 
-    etiqueta_temp_anomalias_string = ttk.Label(text=anomalias_string)
-    etiqueta_temp_anomalias_string.place(x=660, y=140)
+    etiqueta_temp_anomalias_string = ttk.Label(text=anomalias_string, background="bisque")
+    etiqueta_temp_anomalias_string.grid(column=3, row=3, padx=4, pady=4, sticky="nw")
 
     f = open('log_' + bdd.nombre_bdd + '_relaciones.txt', 'w')
     f.write('')
@@ -159,25 +166,27 @@ def analizar_temp():
 
 ventana = tk.Tk()
 ventana.title("Auditoria de anomalias - Grupo 5")
-ventana.config(width=1000, height=600)
-etiqueta_temp_base_de_datos = ttk.Label(text="Ingrese la base de datos:")
-etiqueta_temp_base_de_datos.place(x=20, y=20)
+ventana.config(background="blanched almond")
+
+etiqueta_temp_base_de_datos = ttk.Label(text="Ingrese la base de datos:", background="bisque")
+etiqueta_temp_base_de_datos.grid(column=0, row=0, padx=4, pady=4, sticky="nw")
+
 caja_temp_nombre_bdd = ttk.Entry()
-caja_temp_nombre_bdd.place(x=180, y=20, width=60)
-boton_analizar = ttk.Button(text="Analizar", command=analizar_temp)
-boton_analizar.place(x=20, y=60)
+caja_temp_nombre_bdd.grid(column=1, row=0, padx=4, pady=4, sticky="nw")
 
+boton_analizar = tk.Button(text="Analizar", command=analizar_temp, bg="bisque")
+boton_analizar.grid(column=1, row=1, padx=4, pady=4, sticky="nw")
 
-etiqueta_temp_relaciones_existentes = ttk.Label(text="Relaciones Existentes:")
-etiqueta_temp_relaciones_existentes.place(x=20, y=120)
+etiqueta_temp_relaciones_existentes = ttk.Label(text="Relaciones Existentes:", background="bisque")
+etiqueta_temp_relaciones_existentes.grid(column=0, row=2, padx=4, pady=4, sticky="nw")
 
-etiqueta_temp_relaciones_faltantes = ttk.Label(text="Relaciones Faltantes:")
-etiqueta_temp_relaciones_faltantes.place(x=220, y=120)
+etiqueta_temp_relaciones_faltantes = ttk.Label(text="Relaciones Faltantes:", background="bisque")
+etiqueta_temp_relaciones_faltantes.grid(column=1, row=2, padx=4, pady=4, sticky="nw")
 
-etiqueta_temp_triggers = ttk.Label(text="Triggers:")
-etiqueta_temp_triggers.place(x=420, y=120)
+etiqueta_temp_triggers = ttk.Label(text="Triggers:", background="bisque")
+etiqueta_temp_triggers.grid(column=2, row=2, padx=4, pady=4, sticky="nw")
 
-etiqueta_temp_anomalias = ttk.Label(text="Anomalias de datos:")
-etiqueta_temp_anomalias.place(x=660, y=120)
+etiqueta_temp_anomalias = ttk.Label(text="Anomalias de datos:", background="bisque")
+etiqueta_temp_anomalias.grid(column=3, row=2, padx=4, pady=4, sticky="nw")
 
 ventana.mainloop()
